6f85f6235ddc9328ea548405c32c77b0e9819019
Mouhamad <mhamad.mouh12345@gmail.com>
2024-02-12 14:06:47 +0200

    first commit

--- dist/src/lib/utils.js
+++ dist/src/lib/utils.js
@@ -1,25 +1,28 @@
-import { type ClassValue, clsx } from "clsx";
-import { Metadata } from "next";
-import { twMerge } from "tailwind-merge";
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.formatPrice = exports.cn = void 0;
+var clsx_1 = require("clsx");
+var tailwind_merge_1 = require("tailwind-merge");
+function cn() {
+    var inputs = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        inputs[_i] = arguments[_i];
+    }
+    return (0, tailwind_merge_1.twMerge)((0, clsx_1.clsx)(inputs));
 }
-export function formatPrice(
-  price: number | string,
-  options: {
-    currency?: "USD" | "EUR" | "LBP";
-    notation?: Intl.NumberFormatOptions["notation"];
-  } = {}
-) {
-  const { currency = "USD", notation = "compact" } = options;
-  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    notation,
-    currency,
-    maximumFractionDigits: 2,
-  }).format(numericPrice);
+exports.cn = cn;
+function formatPrice(price, options) {
+    if (options === void 0) { options = {}; }
+    var _a = options.currency, currency = _a === void 0 ? "USD" : _a, _b = options.notation, notation = _b === void 0 ? "compact" : _b;
+    var numericPrice = typeof price === "string" ? parseFloat(price) : price;
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        notation: notation,
+        currency: currency,
+        maximumFractionDigits: 2,
+    }).format(numericPrice);
 }
+exports.formatPrice = formatPrice;
 // export function constructMetadata({
 //   title = "DigitalHippo - the marketplace for digital assets",
 //   description = "DigitalHippo is an open-source marketplace for high-quality digital goods.",
